3e69f36e48f69dec3b7c4b54554304bb16af366f
Ole Jørgen <olejorgenbakken@icloud.com>
2023-03-21 14:39:05 +0100

    added a ton of content

--- data/collections/apps.ts
+++ data/collections/apps.ts
@@ -1,11 +1,84 @@
-import Weight from "@/app/icons/Weight";
-
 export const apps = [
+ {
+  category: "Social media",
+  description: "Apps for social media.",
+  color: "var(--blue-500)",
+  appList: [
+   {
+    name: "Twitter",
+    description: "Elon Musk hellscape.",
+    url: "https://apps.apple.com/us/app/twitter/id333903271",
+   },
+   {
+    name: "Instagram",
+    description: "Mark Zuckerberg hellscape.",
+    url: "https://apps.apple.com/us/app/instagram/id389801252",
+   },
+   {
+    name: "TikTok",
+    description: "The only good social media app.",
+    url: "https://apps.apple.com/us/app/tiktok/id835599320",
+   },
+   {
+    name: "LinkedIn",
+    description: "Workfluence hellscape.",
+    url: "https://apps.apple.com/us/app/reddit-official-app/id1064216828",
+   }
+  ]
+ },
+ {
+  category: "Music",
+  description: "Apps for listening to music and following artists.",
+  color: "var(--red-500)",
+  appList: [
+   {
+    name: "Apple Music",
+    description: "Apple's music streaming service.",
+    url: "https://apps.apple.com/us/app/apple-music/id1108185179",
+   },
+   {
+    name: "Shazam",
+    description: "Yeah I actually use it.",
+    url: "https://apps.apple.com/us/app/shazam/id897118787",
+   },
+   {
+    name: "MusicHarbor",
+    description: "Follow upcoming releases.",
+    url: "https://apps.apple.com/us/app/musicharbor/id1481000001",
+   },
+  ],
+ },
+ {
+  category: "Streaming",
+  description: "Apps for streaming media.",
+  color: "var(--purple-500)",
+  appList: [
+   {
+    name: "Disney+",
+    description: "Disney streaming service.",
+    url: "https://apps.apple.com/us/app/disney/id1427273905",
+   },
+   {
+    name: "Letterboxd",
+    description: "I enjoyed the shoutout in Scream 6.",
+    url: "https://apps.apple.com/us/app/letterboxd-film-journal/id1207158752",
+   },
+   {
+    name: "Apple TV+",
+    description: "Apple streaming service.",
+    url: "https://apps.apple.com/us/app/apple-tv/id1496833156",
+   },
+   {
+    name: "HBO Max",
+    description: "WarnerMedia streaming service.",
+    url: "https://apps.apple.com/us/app/hbo-max/id1480666471",
+   },
+  ],
+ },
  {
   category: "Fitness",
   description: "Apps for tracking fitness activities.",
   color: "var(--green-500)",
-  icon: Weight,
   appList: [
    {
     name: "AutoSleep",
@@ -42,6 +115,11 @@ export const apps = [
     description: "Social fitness tracker.",
     url: "https://apps.apple.com/us/app/strava/id429245458",
    },
+   {
+    name: "Any Distance",
+    description: "Running tracker.",
+    url: "https://apps.apple.com/us/app/any-distance/id1481000002",
+   },
   ]
  },
  {
@@ -54,6 +132,79 @@ export const apps = [
     description: "A fun little game where you shoot down holes in the ground.",
     url: "https://apps.apple.com/us/app/a-dark-room/id1481000009",
    },
+   {
+    name: "Kahoot!",
+    description: "Trivia and group games.",
+    url: "https://apps.apple.com/us/app/kahoot!/id469463298",
+   }
   ]
- }
+ },
+ {
+  category: "Reference",
+  description: "Apps for looking up information.",
+  color: "var(--orange-500)",
+  appList: [
+   {
+    name: "Wikipedia",
+    description: "The free encyclopedia.",
+    url: "https://apps.apple.com/us/app/wikipedia/id324715238",
+   },
+  ],
+ },
+ {
+  category: "Travel",
+  description: "Apps for traveling.",
+  color: "var(--pink-500)",
+  appList: [
+   {
+    name: "Flighty",
+    description: "Flight tracking.",
+    url: "https://apps.apple.com/us/app/flighty/id1481000010",
+   },
+   {
+    name: "Ruter",
+    description: "Public transportation in Oslo.",
+    url: "https://apps.apple.com/us/app/ruter/id1481000011",
+   },
+   {
+    name: "Vy",
+    description: "Public transportation in Norway.",
+    url: "https://apps.apple.com/us/app/vy/id1481000012",
+   },
+  ],
+ },
+ {
+  category: "Work",
+  description: "Apps I use primarily for work.",
+  color: "var(--blue-500)",
+  appList: [
+   {
+    name: "GitHub",
+    description: "Project tracking.",
+    url: "https://apps.apple.com/us/app/github/id1477376905",
+   },
+   {
+    name: "Slack",
+    description: "Work chat.",
+    url: "https://apps.apple.com/us/app/slack/id618783545",
+   },
+  ],
+ },
+ {
+  category: "Finance",
+  description: "Apps for managing my cash.",
+  color: "var(--gray-500)",
+  appList: [
+   {
+    name: "BankID",
+    description: "Government login service.",
+    url: "https://apps.apple.com/us/app/bankid/id1481000013",
+   },
+   {
+    name: "Vipps",
+    description: "Mobile payment.",
+    url: "https://apps.apple.com/us/app/vipps/id1481000014",
+   },
+  ],
+ },
 ]
--- src/app/collections/apps/page.tsx
+++ src/app/collections/apps/page.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import cl from 'clsx';
 import styles from "./page.module.css";
 
@@ -16,14 +17,14 @@ export default function Apps() {
     <p>A collection of apps I always download when I get a new device.</p>
    </header>
    <div className={styles.apps}>
-    {apps.map((group) => (
+    {apps.sort((a, b) => (a.category > b.category) ? 1 : ((b.category > a.category) ? -1 : 0)).map((group) => (
      <div key={group.category} className={cl(styles.category)}>
       <header>
        <h2 className={cl(styles.categoryTitle)}>{group.category}</h2>
        {group.description && <p className={cl(styles.description)}>{group.description}</p>}
       </header>
       <ul className={cl(styles.appList)}>
-       {group.appList.map((app) => (
+       {group.appList.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).map((app) => (
         <AppCard {...app} key={app.name} />
        ))}
       </ul>
