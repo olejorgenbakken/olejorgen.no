4ccca4750ec741fd286905ccb0e62cd8960708ff
Ole Jørgen <olejorgenbakken@icloud.com>
2023-03-21 07:40:28 +0100

    fixes Job props

--- src/app/componentes/CV/job/job.tsx
+++ src/app/componentes/CV/job/job.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 interface JobProps {
  title: string;
  company: string;
- description: string;
+ description?: string;
  date: {
   start: string;
   end?: string;
@@ -18,7 +18,6 @@ function Job({ title, company, description, date, url }: JobProps) {
   <li className={cl(styles.job)}>
    <h3 className={styles.title}>{title} <span className={cl(styles.company)}>at <Link href={url} title={`Go to ${company}`} target="_blank" rel="noopener noreferrer">{company}</Link></span></h3>
    <p className={cl(styles.description)}><span className={cl(styles.dates)}>From {date.start}{date.end && ` to ${date.end}`}.</span> {description}</p>
-   {/* <p className={cl(styles.link)}><Link href={url} title={`Go to ${company}`} target="_blank" rel="noopener noreferrer">Visit {company}</Link></p> */}
   </li>
  );
 }
