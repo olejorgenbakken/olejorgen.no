b64392c38dd8428b9ac60a32359c40e277a4a511
Ole Jørgen <ole.jorgen.bakken@fremtind.no>
2023-11-26 22:01:07 +0100

    dark mode -.-

--- app/components/layout/Tabs.tsx
+++ app/components/layout/Tabs.tsx
@@ -17,7 +17,7 @@ export default function Tabs({ linkList }: TabsProps) {
    <ul className="flex flex-row gap-4">
     {linkList.map((link, index) => (
      <li key={index}>
-      <Link href={link.href} className={`tab rounded-full px-4 py-2 border border-zinc-300 ${pathname === link.href ? " bg-slate-200" : ""}`}>{link.name}</Link>
+      <Link href={link.href} className={`tab rounded-full px-4 py-2 border border-zinc-300 dark:border-zinc-600 ${pathname === link.href ? "font-bold bg-zinc-200 dark:bg-zinc-800" : ""}`}>{link.name}</Link>
      </li>
     ))}
    </ul>
