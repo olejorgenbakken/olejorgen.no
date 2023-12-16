36b9a0554dfe037731fa2bcfbda32ec7d083a12d
Ole Jørgen <ole.jorgen.bakken@fremtind.no>
2023-12-16 20:58:13 +0100

    la til speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: 'Ole Jørgen Bakken',
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="no">
       <body>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
