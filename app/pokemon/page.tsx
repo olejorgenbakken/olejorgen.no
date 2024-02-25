a0f183de60dc04996ed9e81e16077bcb9af4a816
Ole Jørgen <37940048+olejorgenbakken@users.noreply.github.com>
2024-02-25 01:07:32 +0100

    ::DDD

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -5,25 +5,9 @@ import { useEffect, useState } from "react";
 import Main from "../components/Layout/Main/Main";
 import PokemonList from "../components/Pokemon/PokemonList";
 import Heading from "../components/Text/Heading/Heading";
+import { fetchPokemon, fetchPokemonCount } from "../data/pokemon";
 
 import type { Pokemon } from "../models/pokemon.model";
-function fetchPokemonCount(): Promise<any> {
-    return fetch("https://pokeapi.co/api/v2/pokemon?limit=1600")
-        .then((response) => response.json())
-        .then((data) => {
-            return data.count;
-        });
-}
-
-export function fetchPokemon(id: number): Promise<Pokemon> {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            return data;
-        });
-}
-
 export default function Home() {
     const [pokemonCount, setPokemonCount] = useState<any | null>(null);
     const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
