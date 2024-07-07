faec46224ef785da433292e7b8406bb4153064dd
Ole Jørgen <37940048+olejorgenbakken@users.noreply.github.com>
2024-07-07 05:10:46 +0200

    fjern tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -4,15 +4,17 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+} from '@remix-run/react';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
         <Meta />
         <Links />
       </head>
