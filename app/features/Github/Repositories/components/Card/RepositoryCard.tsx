e7e7e0bba572ee738aa593d1f14d060fd579a0d6
Ole Jørgen <37940048+olejorgenbakken@users.noreply.github.com>
2024-07-10 23:08:06 +0200

    over 0 lol

--- app/features/Github/Repositories/components/Card/RepositoryCard.tsx
+++ app/features/Github/Repositories/components/Card/RepositoryCard.tsx
@@ -26,7 +26,7 @@ export default async function RepositoryCard({ repository }: RepoProps) {
         <p className="topics">{topics?.map((topic) => topic).join(', ')}</p>
       </header>
       {description && <p className="description">{description}</p>}
-      {commits.total && (
+      {commits.total > 0 && (
         <p className="commits">
           <small>Commits: {commits.total}</small>
         </p>
